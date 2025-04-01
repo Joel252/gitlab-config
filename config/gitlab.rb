41f18e7d3fd87984abc6fa5ca8ae1d9a6d1111f9
Joel252 <joel.enrique.cruz860@gmail.com>
2025-04-01 03:32:39 -0500

    fix: variable syntax correction

--- config/gitlab.rb
+++ config/gitlab.rb
@@ -1,132 +1,97 @@
-external_url 'https://${GITLAB_DOMAIN}'
-nginx['listen_https'] = true
-nginx['redirect_http_to_https'] = true
+# For more options, visit:
+# https://gitlab.com/gitlab-org/omnibus-gitlab/blob/master/files/gitlab-config-template/gitlab.rb.template
 
-{% if SSL_MODE == "letsencrypt" %}
-letsencrypt['enable'] = true
-letsencrypt['contact_emails'] = ['${LETSENCRYPT_EMAIL}']
+external_url ENV['GITLAB_EXTERNAL_URL']
+
+# --- SSL Configuration ---
+letsencrypt['enable'] = ENV['LETSENCRYPT_ENABLED'] == "true" # true to use Let's Encrypt
+letsencrypt['contact_emails'] = ["admin@#{ENV['GITLAB_DOMAIN']}"]
 letsencrypt['auto_renew'] = true
-{% elif SSL_MODE == "custom" %}
-nginx['ssl_certificate'] = "/etc/gitlab/ssl/${SSL_CERT_FILE}"
-nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/${SSL_KEY_FILE}"
-{% else %}
-nginx['listen_https'] = false
-nginx['redirect_http_to_https'] = false
-nginx['listen_port'] = 80
-gitlab_rails['gitlab_https'] = true
-{% endif %}
 
+nginx['listen_https'] = ENV['ENABLE_SSL'] == "true"
+nginx['listen_port'] = 80 unless ENV['ENABLE_SSL'] == "true" # If SSL is disabled, listen on HTTP
+nginx['redirect_http_to_https'] = ENV['ENABLE_SSL'] == "true"
+nginx['ssl_certificate'] = "/etc/gitlab/ssl/#{ENV['GITLAB_DOMAIN']}.crt"
+nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/#{ENV['GITLAB_DOMAIN']}.key"
 
-#############################
-# GitLab Database
-#############################
+# --- SSH Configuration ---
+gitlab_rails['gitlab_shell_ssh_port'] = 22
 
-{% if USE_EXTERNAL_DB == "true" %}
-postgresql['enable'] = false
-gitlab_rails['db_adapter'] = 'postgresql'
-gitlab_rails['db_encoding'] = 'unicode'
-gitlab_rails['db_database'] = '${EXTERNAL_DB_NAME}'
-gitlab_rails['db_host'] = '${EXTERNAL_DB_HOST}'
-gitlab_rails['db_username'] = '${EXTERNAL_DB_USER}'
-gitlab_rails['db_password'] = '${EXTERNAL_DB_PASS}'
-{% else %}
-postgresql['enable'] = true
-{% endif %}
+# --- Database Configuration ---
+postgresql['enable'] = ENV['USE_INTERNAL_DB']
+gitlab_rails['db_adapter'] = "postgresql"
+gitlab_rails['db_database'] = ENV['DB_NAME']
+gitlab_rails['db_host'] = ENV['DB_HOST']
+gitlab_rails['db_port'] = 5432
+gitlab_rails['db_username'] = ENV['DB_USER']
+gitlab_rails['db_password'] = ENV['DB_PASS']
 
-#############################
-# S3 BUCKET STORAGES
-#############################
+# --- Redis Configuration ---
+redis['enable'] = ENV['USE_INTERNAL_REDIS'] == "true"
+gitlab_rails['redis_host'] = ENV['REDIS_HOST']
+gitlab_rails['redis_port'] = 6379
 
-# Setup S3 connection for object storage
-gitlab_rails[́́́'object_store']['enabled'] = true
+# --- Object Storage Configuration ---
+gitlab_rails['object_store']['enabled'] = ENV['OBJECT_STORAGE_ENABLED'] == "true"
 gitlab_rails['object_store']['connection'] = {
-    'provider' => 'AWS',
-    'region' => '${AWS_REGION}',
-    'use_iam_profile' => true
+  'provider' => 'AWS',
+  'region' => ENV['S3_REGION'],
+  'aws_access_key_id' => ENV['S3_ACCESS_KEY'],
+  'aws_secret_access_key' => ENV['S3_SECRET_KEY'],
+  'endpoint' => ENV['S3_ENDPOINT'],
 }
-gitlab_rails['object_store']['objects']['artifacts']['enabled'] = true
-gitlab_rails['object_store']['objects']['artifacts']['bucket'] = '${S3_BUCKET_ARTIFACTS}'
-gitlab_rails['object_store']['objects']['lfs']['enabled'] = true
-gitlab_rails['object_store']['objects']['lfs']['bucket'] = '${S3_BUCKET_LFS}'
+gitlab_rails['object_store']['bucket'] = ENV['S3_BUCKET']
 
-# Setup Backup configuration
-gitlab_rails['backup_upload'] = true
-gitlab_rails['backup_upload_connection'] = {
-    'provider' => 'AWS',
-    'region' => '${AWS_REGION}',
-    'use_iam_profile' => true
-}
-gitlab_rails['backup_upload_remote_directory'] = '${S3_BUCKET_BACKUPS}'
+# --- Backup Configuration ---
 gitlab_rails['backup_keep_time'] = 604800 # Delete backups after 7 days (in seconds)
 gitlab_rails['backup_multipart_chunk_size'] = 104857600 # Use multipart uploads for files larger than 100MB
 
+if ENV['BACKUP_METHOD'] == "s3"
+  gitlab_rails['backup_upload_connection'] = {
+    'provider' => 'AWS',
+    'region' => ENV['S3_REGION'],
+    'aws_access_key_id' => ENV['S3_ACCESS_KEY'],
+    'aws_secret_access_key' => ENV['S3_SECRET_KEY'],
+    'endpoint' => ENV['S3_ENDPOINT'],
+  }
+  gitlab_rails['backup_upload_remote_directory'] = ENV['S3_BACKUP_BUCKET']
+elsif ENV['BACKUP_METHOD'] == "local"
+  gitlab_rails['backup_path'] = "/var/opt/gitlab/backups"
+  gitlab_rails['backup_archive_permissions'] = 0644
+end
 
-#############################
-# SMTP SETTINGS
-#############################
-
-# Setup SMTP connection for email notifications
+# --- SMTP Configuration ---
 gitlab_rails['smtp_enable'] = true
-gitlab_rails['smtp_address'] = '${SMTP_SERVER}'
-gitlab_rails['smtp_domain'] = '${SMTP_DOMAIN}'
-gitlab_rails['smtp_port'] = '${SMTP_PORT}'
-gitlab_rails['smtp_user_name'] = '${SMTP_USER}'
-gitlab_rails['smtp_password'] = '${SMTP_PASS}'
-gitlab_rails['smtp_authentication'] = '${SMTP_AUTH}'
+gitlab_rails['smtp_address'] = ENV['SMTP_HOST']
+gitlab_rails['smtp_port'] = 587
+gitlab_rails['smtp_user_name'] = ENV['SMTP_USER']
+gitlab_rails['smtp_password'] = ENV['SMTP_PASS']
+gitlab_rails['smtp_domain'] = ENV['SMTP_DOMAIN']
+gitlab_rails['smtp_authentication'] = "login"
 gitlab_rails['smtp_enable_starttls_auto'] = true
-gitlab_rails['smtp_tls'] = false
-gitlab_rails['smtp_openssl_verify_mode'] = 'peer'
 gitlab_rails['gitlab_email_enabled'] = true
-gitlab_rails['gitlab_email_from'] = '${SMTP_USER}'
-gitlab_rails['gitlab_email_reply_to'] = '${SMTP_USER}'
+gitlab_rails['gitlab_email_from'] = ENV['SMTP_USER']
+gitlab_rails['gitlab_email_display_name'] = ENV['EMAIL_DISPLAY_NAME']
+gitlab_rails['gitlab_email_reply_to'] = ENV['SMTP_USER']
 
-
-#############################
-# GitLab Logging 
-#############################
-
-logging['svlogd_size'] = 200 * 1024 * 1024 # rotate after 200 MB of log data
-logging['svlogd_num'] = 30 # keep 30 rotated log files
-logging['svlogd_timeout'] = 24 * 60 * 60 # rotate after 24 hours
+# --- Logrotate ---
+# See: https://gitlab.com/gitlab-org/omnibus-gitlab/tree/master/README.md#logrotate
+logrotate['enable'] = true
+logging['logrotate_frequency'] = "daily"
+logging['logrotate_size'] = ENV['LOGROTATE_SIZE']
 logging['svlogd_filter'] = "gzip" # compress logs with gzip
-logging['svlogd_udp'] = nil # transmit log messages via UDP
-logging['svlogd_prefix'] = nil # custom prefix for log messages
-logging['logrotate_frequency'] = "daily" # rotate logs daily
-logging['logrotate_maxsize'] = nil # logs will be rotated when they grow bigger than size specified for `maxsize`, even before the specified time interval (daily, weekly, monthly, or yearly)
-logging['logrotate_size'] = nil # do not rotate by size by default
-logging['logrotate_rotate'] = 30 # keep 30 rotated logs
-logging['logrotate_compress'] = "compress" # see 'man logrotate'
-logging['logrotate_method'] = "copytruncate" # see 'man logrotate'
-logging['logrotate_postrotate'] = nil # no post-rotate command by default
-logging['logrotate_dateformat'] = nil # use date extensions for rotated files rather than numbers e.g. a value of "-%Y-%m-%d" would give rotated files like production.log-2016-03-09.gz
+logging['svlogd_size'] = 200 * 1024 * 1024 # rotate after 200 MB of log data
+logging['svlogd_timeout'] = 24 * 60 * 60 # rotate after 24 hours
 
-
-#############################
-# GitLab Container Registry
-#############################
-
+# --- GitLab Registry ---
+registry_external_url ENV['GITLAB_REGISTRY_URL']
 gitlab_rails['registry_enabled'] = true
-registry_external_url 'https://registry-${GITLAB_DOMAIN}'
-registry_nginx['listen_https'] = true
-registry_nginx['redirect_http_to_https'] = true
+gitlab_rails['registry_host'] = ENV['GITLAB_REGISTRY_DOMAIN']
+gitlab_rails['registry_api_url'] = "#{ENV['GITLAB_REGISTRY_URL']}/api"
+gitlab_rails['registry_key_path'] = "/etc/gitlab/ssl/registry.key"
 
-{% if SSL_MODE == "letsencrypt" %}
-letsencrypt['enable'] = true
-letsencrypt['contact_emails'] = ['${LETSENCRYPT_EMAIL}']
-letsencrypt['auto_renew'] = true
-{% elif SSL_MODE == "custom" %}
-registry_nginx['ssl_certificate'] = "/etc/gitlab/ssl/${SSL_CERT_FILE}"
-registry_nginx['ssl_certificate_key'] = "/etc/gitlab/ssl/${SSL_KEY_FILE}"
-{% else %}
-registry_nginx['listen_https'] = false
-registry_nginx['redirect_http_to_https'] = false
-registry_nginx['listen_port'] = 5050
-{% endif %}
-
-
-#############################
-# GitLab Runner
-#############################
-
-# Register token for runners
-gitlab_rails['ci_runner_registration_token'] = '${GITLAB_RUNNER_TOKEN}'
+# --- GitLab Pages ---
+pages_external_url ENV['GITLAB_PAGES_URL']
+gitlab_pages['enable'] = true
+gitlab_pages['external_http'] = ['0.0.0.0:80'] unless ENV['ENABLE_SSL'] == "true"
+gitlab_pages['external_https'] = ['0.0.0.0:443'] if ENV['ENABLE_SSL']  == "true"
